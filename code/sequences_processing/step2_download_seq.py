ccb207a81da8e7d4776075443ea77f627b603c0a
lhao <lhao@chalmers.se>
2020-02-21 15:29:48 +0100

    fix: fix ftp_path==na error and write into 'Error_final_1.txt'; ignore two species that with too many stains/seqs (Escherichia coli; Klebsiella pneumoniae)

--- code/sequences_processing/step2_download_seq.py
+++ code/sequences_processing/step2_download_seq.py
@@ -10,7 +10,7 @@
 """
 
 import os
-
+import time
 import pandas as pd
 import wget
 
@@ -20,22 +20,38 @@ os.chdir('../../data/sequences_processing/')
 refseq_info_df = pd.read_csv('species_info.txt', sep='\t')
 
 # %% download:
-# list_1 = os.listdir('sequences_process2/')
-list_1 = []
+# list_old = set(os.listdir('sequences2/'))
+list_old = []
+list_final = set(os.listdir('sequences/'))
 
+
+f = open('Error_final_1.txt', 'w')
 for index in range(0, refseq_info_df.shape[0]):  #
     # refseq_info_df.columns
     # refseq_info_df['# assembly_accession']
     # refseq_info_df.ftp_path
 
+    # ignore the species that have too many seqs/strains: Escherichia coli (18000+); Klebsiella pneumoniae (8000+)
+    if ('Escherichia coli' in refseq_info_df.iloc[index]['organism_name'] )or 'Klebsiella pneumoniae' in refseq_info_df.iloc[index]['organism_name']:
+        print(index, end=' passed \t')
+        if index % 5 == 0:
+            print('')
+        continue
+
     ftp_path = refseq_info_df.iloc[index]['ftp_path']
     faa_name = ftp_path.split('/')[-1] + '_protein.faa.gz'
     fna_name = ftp_path.split('/')[-1] + '_genomic.fna.gz'
 
+    if faa_name in list_final and fna_name in list_final:
+        print(index, end=' downloaded \t')
+        if index % 5 == 0:
+            print('')
+        continue
+
     # strain_name = refseq_info_df.iloc[index]['organism_name']
     # strain_name = refseq_info_df.iloc[index]['infraspecific_name'].split('=')[-1]
     try:
-        if faa_name not in list_1:
+        if faa_name not in list_old:
             file_url_faa = ftp_path + '/' + faa_name
             wget.download(file_url_faa, 'sequences/')
         else:
@@ -45,7 +61,25 @@ for index in range(0, refseq_info_df.shape[0]):  #
         wget.download(file_url_fna, 'sequences/')
         # os.rename('sequience/'+ftp_path.split('/')[-1] + '_genomic.gbff', os.path.join('all_Lreu_strains_gbff/', strain_name+ '.gbff'))
         # os.remove(my_file)
-        print(index)
+        print(index, end='\t')
+        if index % 5 == 0:
+            print('')
     except:
-        raise
-        print('ERROR!!!   ' + file_url)
+        #raise
+        f.write('ERROR!!!\t' + str(index) + '\t' +ftp_path)
+        f.flush()
+        print('ERROR!!!\t' + str(index) + '\t' +ftp_path)
+
+    if (index ) % 50 == 0:
+        print('sleeping')
+        time.sleep(0)
+        # if (index ) % 100 == 0:
+        #     print('sleeping')
+        #     time.sleep(129)
+
+f.close()
+print('Done')
+# all: 32466
+# 818
+# 819
+# 19114
