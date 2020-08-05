b128bd211a9dabc419717751a74092d47851d4b8
lhao <lhao@chalmers.se>
2020-08-05 08:35:26 +0200

    feat: generate_biomass_equation from map table into txt

--- code/biomass/generate_biomass_equation.py
+++ code/biomass/generate_biomass_equation.py
@@ -3,10 +3,10 @@
 # Created by Hao Luo at 8/4/20
 
 """generate_biomass_quation.py
-:description : script
+:description : get a models contain biomass reactions from a table
 :param : 
-:returns: 
-:rtype: 
+:returns: model
+:rtype: txt and json
 """
 
 import os
@@ -16,8 +16,27 @@ import pandas as pd
 
 os.chdir('../../data/biomass/')
 
+input_file = 'biomass_constituents_id_map.tsv'
+output_file_n = 'biomass_negative_reactions.txt'
+output_file_p = 'biomass_positive_reactions.txt'
+output_file_a = 'biomass_archaea_reactions.txt'
+
 
 def get_biomass_model(dic_i, observations_list=False):
+    '''
+    :description : function to get a model contain biomass reactions from a dic
+    :param :
+        dic_i(dic) : {'met': [coefficient, 'pool]}
+                    eg. {'10-FORMYL-THF': [-0.000367, 'cofactor'],
+                     'AMP': [-0.0046700000000000005, 'cofactor'],
+                     'CPD-12125': [-0.000266, 'cofactor'],
+                     'CDP': [-0.000251, 'cofactor'],}
+        observations_list like:
+            eg. ['dna', 'rna', 'protein', 'lipid', 'carbohydrate', 'cofactor', 'ion', 'other']
+
+    :returns: cobra model
+    '''
+
     if observations_list == False:
         observations_list = ['dna', 'rna', 'protein', 'lipid', 'carbohydrate', 'cofactor', 'ion', 'other']
     observations_list = [i + '_c' for i in observations_list]
@@ -33,18 +52,17 @@ def get_biomass_model(dic_i, observations_list=False):
 
     for k, v in dic_i.items():
         # print(k)
-        try:
+        try:  # build metabolites
             met = biomass_model.metabolites.get_by_id(k)
         except:
-
             met = cobra.Metabolite(k)
-        try:
+        try:  # build reactions
             reaction_i = biomass_model.reactions.get_by_id('Biomass_' + v[1] + '_c')
             reaction_i.add_metabolites({met: v[0]})
         except:
             print(k, v, 'Error')
 
-    # %% <Biomass reactions>
+    # %% <Biomass reaction>
     Biomass = cobra.Reaction('Biomass')
     [Biomass.add_metabolites({biomass_model.metabolites.get_by_id(met): -1}) for met in observations_list]
     Biomass.add_metabolites({cobra.Metabolite('Biomass'): 1})
@@ -55,7 +73,7 @@ def get_biomass_model(dic_i, observations_list=False):
 
 # %% <add biomass to models>
 
-biomass_df = pd.read_csv('biomass_constituents_id_map.tsv', sep='\t', header=0, index_col=0)
+biomass_df = pd.read_csv(input_file, sep='\t', header=0, index_col=0)
 observations_list = ['dna', 'rna', 'protein', 'lipid', 'carbohydrate', 'cofactor', 'ion', 'other']
 
 # %%<gram positive>
@@ -79,11 +97,17 @@ archaea_dic = archaea_df.set_index('metacyc_id').T.to_dict('list')
 
 model_a = get_biomass_model(archaea_dic, observations_list)
 
+# %% <write model>
 import gemstool
 
-gemstool.io.gem2txt(model_n, 'biomass_negative_reactions.txt')
-gemstool.io.gem2txt(model_p, 'biomass_positive_reactions.txt')
-gemstool.io.gem2txt(model_a, 'biomass_archaea_reactions.txt')
+gemstool.io.gem2txt(model_n, output_file_n)  # write corba model to txt file
+gemstool.io.gem2txt(model_p, output_file_p)
+gemstool.io.gem2txt(model_a, output_file_a)
+
+cobra.io.save_json_model(model_n, output_file_n.replace('txt', 'json'))  # write json model, easy import by python to
+cobra.io.save_json_model(model_p, output_file_p.replace('txt', 'json'))
+cobra.io.save_json_model(model_a, output_file_a.replace('txt', 'json'))
+
 # #
 # # gram_n_df = gram_n_df[(gram_n_df['metacyc']!='')]
 # # gram_p_df = gram_p_df[(gram_p_df['metacyc']!='')]
@@ -102,15 +126,3 @@ gemstool.io.gem2txt(model_a, 'biomass_archaea_reactions.txt')
 # #         reaction_s.append(str(-v) + ' ' + k)
 # # reaction = ' + '.join(reaction_s) + ' --> '+ ' + '.join(reaction_p)
 # # biomass_n.reaction = reaction
-#
-# biomass_n.reaction = ' --> '
-# for k,v in gram_n_dic.items():
-#     # print(k)
-#     try:
-#         met = model.metabolites.get_by_id(k)
-#     except :
-#         print(met)
-#         met = cobra.Metabolite(k)
-#
-#     biomass_n.add_metabolites({met:v})
-#
