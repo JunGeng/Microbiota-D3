b9a0b8324832465a9b9252c7cc791be8a7a8d379
lhao <lhao@chalmers.se>
2020-08-19 17:11:10 +0200

    feat: add functions dir

---
+++ code/Functions/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Created by Hao Luo at 8/19/20
+
+"""__init__.py
+:description : script
+:param : 
+:returns: 
+:rtype: 
+"""
+import dna
+import rna
