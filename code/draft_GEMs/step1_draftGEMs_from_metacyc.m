31d79a0d7cd6577d19d2f0bfa67eb230348baddf
lhao <lhao@chalmers.se>
2020-08-25 10:21:01 +0200

    fix: fix species name contain '.' model output files error.

--- code/draft_GEMs/step1_draftGEMs_from_metacyc.m
+++ code/draft_GEMs/step1_draftGEMs_from_metacyc.m
@@ -7,17 +7,22 @@ for seq_index = 1:length(seqs_list)
     species_name = strsplit(seq_name,'_protein.faa');
     species_name = species_name{1};
     model_name = strcat(species_name,'_Metacyc');
-    seq_index
-    model_name
-    
-    MetaCycDraftModel_i=getMetaCycModelForOrganism(model_name,strcat(...
-             'data/sequences_processing/protein_sequences/',seq_name),1);
-    
-    file_name = strcat('data/draft_GEMs/',model_name)
-    exportModel(MetaCycDraftModel_i,file_name,true,false);
-    
-    writeYaml(MetaCycDraftModel_i,file_name)
-    
-    save(file_name,'MetaCycDraftModel_i')
+    %if contains(species_name,'.')
+        species_name = strsplit(seq_name,'_protein.faa');
+        species_name = species_name{1};
+        model_name = strcat(species_name,'_Metacyc');
+        seq_index
+        model_name
+
+        MetaCycDraftModel_i=getMetaCycModelForOrganism(model_name,strcat(...
+                 'data/sequences_processing/protein_sequences/',seq_name),1);
+
+        file_name = strcat('data/draft_GEMs/draft_from_RAVEN_metacyc23_5/',model_name)
+        exportModel(MetaCycDraftModel_i,file_name,true,false);
+
+        writeYaml(MetaCycDraftModel_i,strcat(file_name,'.yaml'))
+
+        save(strcat(file_name,'.mat'),'MetaCycDraftModel_i')
+    %end
         
 end
